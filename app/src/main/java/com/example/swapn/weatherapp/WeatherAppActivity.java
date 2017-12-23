a633fb7693f1aaaa02660c8af4d53f0dfdb3f96d
Swapnil-N <swapnil.napuri@gmail.com>
2017-12-23 12:33:23 -0500

    Able to send, parse, and pull temperature and description from JSON object

--- app/src/main/java/com/example/swapn/weatherapp/WeatherAppActivity.java
+++ app/src/main/java/com/example/swapn/weatherapp/WeatherAppActivity.java
@@ -3,7 +3,14 @@ package com.example.swapn.weatherapp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -18,12 +25,30 @@ public class WeatherAppActivity extends AppCompatActivity {
 
     JSONObject CurrentWeather;
 
+    EditText editText;
+    Button button;
+    TextView textView;
+
+    String zipcode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_app);
 
+        editText = (EditText) findViewById(R.id.id_editText);
+        button = (Button) findViewById(R.id.id_button);
+        textView = (TextView) findViewById(R.id.id_textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zipcode = editText.getText().toString();
+            }
+        });
+
         AsyncThread weatherThread = new AsyncThread();
+        weatherThread.execute();
 
 
     }
@@ -34,7 +59,7 @@ public class WeatherAppActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
 
             try {
-                URL Currenturl = new URL("http://api.openweathermap.org/data/2.5/weather?zip=08852&APPID=5244998c89fd54420cc25028d86b55e8");
+                URL Currenturl = new URL("http://api.openweathermap.org/data/2.5/weather?zip=08810&APPID=5244998c89fd54420cc25028d86b55e8");
                 URLConnection urlConnection = Currenturl.openConnection();
                 InputStream inputStream = urlConnection.getInputStream();
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
@@ -43,8 +68,25 @@ public class WeatherAppActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            //print from JSON object
+            try {
+
+                JSONObject Ctemp = new JSONObject(CurrentWeather.getString("main"));
+                JSONObject thing = new JSONObject(String.valueOf(CurrentWeather.getJSONArray("weather").get(0)));
+                textView.setText(Ctemp.getString("temp")+ "   "+ thing.getString("description"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+
         }
     }
 
